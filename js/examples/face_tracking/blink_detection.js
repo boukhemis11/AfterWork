85283d0ecfc25da048b99154f2a94ec2bce6b6ff
boukhemis11 <boukhemis2011@gmail.com>
2018-05-09 10:10:23 +0200

    fixed

--- js/examples/face_tracking/blink_detection.js
+++ js/examples/face_tracking/blink_detection.js
@@ -1,59 +1,7 @@
 (function exampleCode() {
 	"use strict";
 
-	// get elements
-const startButton = document.querySelector('[data-action="start"]');
-const stopButton = document.querySelector('[data-action="stop"]');
-const resetButton = document.querySelector('[data-action="reset"]');
-const minutes = document.querySelector(".minutes");
-const seconds = document.querySelector(".seconds");
-const milliSeconds = document.querySelector(".milli-seconds");
-// variables
-let timerTime = 00;
-let isRunning = false;
-let interval;
 
-// functions
-function incrementTimer() {
-  timerTime++;
-
-  const numOfMilliSeconds = timerTime % 100;
-  const numOfSeconds = Math.floor((timerTime / 100) % 60);
-  const numOfMinutes = Math.floor(timerTime / 6000);
-
-  milliSeconds.innerText = pad(numOfMilliSeconds);
-  seconds.innerText = pad(numOfSeconds);
-  minutes.innerText = pad(numOfMinutes);
-}
-
-function startTimer() {
-  if (isRunning) return;
-  isRunning = true;
-  interval = setInterval(incrementTimer, 10);
-}
-
-function stopTimer() {
-  if (!isRunning) return;
-  isRunning = false;
-  clearInterval(interval);
-}
-
-function resetTimer() {
-  stopTimer();
-  timerTime = 00;
-  milliSeconds.innerText = '00';
-  seconds.innerText = '00';
-  minutes.innerText = '00';
-}
-
-function pad(number) {
-  return number < 10 ? '0' + number : number;
-  // if (number < 10) {
-  //   return '0' + number;
-  // } else {
-  //   return number;
-  // }
-}
 
 
 	
@@ -178,10 +126,7 @@ function pad(number) {
 
 	brfv4Example.dom.updateCodeSnippet(exampleCode + "");
 
-	// Event listeners
-startButton.addEventListener('click', startTimer);
-stopButton.addEventListener('click', stopTimer);
-resetButton.addEventListener('click', resetTimer);
+
 
 	
 })();
