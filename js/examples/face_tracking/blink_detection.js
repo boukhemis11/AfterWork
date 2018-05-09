32a0d88eda24829dddbb29b564a61bc1f6154723
mohamedammi <33319754+mohamedammi@users.noreply.github.com>
2018-05-09 11:15:49 +0200

    ajout chrono

--- js/examples/face_tracking/blink_detection.js
+++ js/examples/face_tracking/blink_detection.js
@@ -92,7 +92,7 @@
 				draw.drawVertices(	face.vertices, 2.0, false, color, 0.4);
 
 				brfv4Example.dom.updateHeadline("BRFv4 - advanced - face tracking - simple blink" +
-					"detection.\nDetects an eye  blink: " + (_blinked ? "Yes" : "No"));
+					"detection.\nDetects an eye  blink: " + (_blinked ? clearTimeout(t) : "No"));
 
 				storeFaceShapeVertices(v);
 			}
@@ -126,7 +126,47 @@
 
 	brfv4Example.dom.updateCodeSnippet(exampleCode + "");
 
+	var h1 = document.getElementsByTagName('h1')[0],
+    start = document.getElementById('start'),
+    stop = document.getElementById('stop'),
+    clear = document.getElementById('clear'),
+    seconds = 0, minutes = 0, hours = 0,
+    t;
 
+function add() {
+    seconds++;
+    if (seconds >= 60) {
+        seconds = 0;
+        minutes++;
+        if (minutes >= 60) {
+            minutes = 0;
+            hours++;
+        }
+    }
+    
+    h1.textContent = (hours ? (hours > 9 ? hours : "0" + hours) : "00") + ":" + (minutes ? (minutes > 9 ? minutes : "0" + minutes) : "00") + ":" + (seconds > 9 ? seconds : "0" + seconds);
+
+    timer();
+}
+function timer() {
+    t = setTimeout(add, 1000);
+}
+timer();
+
+
+/* Start button */
+start.onclick = timer;
+
+/* Stop button */
+stop.onclick = function() {
+    clearTimeout(t);
+}
+
+/* Clear button */
+clear.onclick = function() {
+    h1.textContent = "00:00:00";
+    seconds = 0; minutes = 0; hours = 0;
+}
 
 	
 })();
