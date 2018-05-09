e7e4afd8fbcc310f0a5eba25534b8dd840818204
boukhemis11 <boukhemis2011@gmail.com>
2018-05-09 16:41:47 +0200

    fix

--- js/examples/face_tracking/blink_detection.js
+++ js/examples/face_tracking/blink_detection.js
@@ -104,7 +104,7 @@
 
 		if(_timeOut > -1) { clearTimeout(_timeOut); }
 
-		_timeOut = setTimeout(resetBlink, 150);
+		_timeOut = setTimeout(resetBlink, 1);
 	}
 
 	function resetBlink() {
@@ -132,29 +132,34 @@
     stop = document.getElementById('stop'),
     clear = document.getElementById('clear'),
 	score = document.getElementById('score'),
-    milliseconde=0,seconds = 0, minutes = 0, hours = 0,
+    milliseconde = 0 ,seconds = 0, minutes = 0, 
+    t;
+
+	var h1 = document.getElementsByTagName('h1')[0],
+    start = document.getElementById('start'),
+    stop = document.getElementById('stop'),
+    clear = document.getElementById('clear'),
+    mils = 0 ,seconds = 0, minutes = 0, hours = 0,
     t;
 
 function add() {
-    milliseconde++;
-	if(milliseconde>=1000){
-		milliseconde = 0;
-		if (seconds >= 60) {
-			seconds = 0;
-			minutes++;
-			if (minutes >= 60) {
-				minutes = 0;
-				hours++;
-			}
-		}
-	}
+
+    mils++;
+    if (mils >= 100) {
+        mils = 0;
+        seconds++;
+    if (seconds >= 60) {
+        seconds = 0;
+        minutes++;
+    }
+  }
     
-    h1.textContent = (hours ? (hours > 9 ? hours : "0" + hours) : "00") + ":" + (minutes ? (minutes > 9 ? minutes : "0" + minutes) : "00") + ":" + (seconds > 9 ? seconds : "0" + seconds)+ ":" + (milliseconde > 999 ? milliseconde : "0" + milliseconde);
+    h1.textContent =(minutes ? (minutes > 9 ? minutes : "0" + minutes) : "00") + ":" + (seconds > 9 ? seconds : "0" + seconds)+ ":" + (mils > 9 ? mils : "0" + mils);
 
     timer();
 }
 function timer() {
-    t = setTimeout(add, 1);
+    t = setTimeout(add, 10);
 }
 
 
@@ -165,23 +170,38 @@ start.onclick = timer;
 /* Stop button */
 stop.onclick = function() {
     clearTimeout(t);
-	timeR();
 }
 
 /* Clear button */
 clear.onclick = function() {
     h1.textContent = "00:00:00";
-    milliseconde = 0; seconds = 0; minutes = 0; hours = 0;
+    seconds = 0; minutes = 0; hours = 0;
 }
 
 function timeR(){
+
 	clearTimeout(t);
-	if(tabTime.length<5){
-  	tabTime.push(h1.textContent);
-    
-    }
-    tabTime.sort();
-var tab = tabTime.join('-');
+
+	
+				if (h1.textContent != "00:00:00" ){
+				tabTime.push(h1.textContent);
+				seconds = 0; minutes = 0; hours = 0;
+				h1.textContent = "00:00:00";
+				
+
+			}
+
+	tabTime.sort();
+/*	
+for(var i = 0; i<5; i++){
+	var tab2;
+	var k = tabTime[i];
+	push(k).tab2
+}
+*/
+
+var tab = tabTime.slice(-5);
+tab.reverse();
 score.textContent= tab;
 }
 	
